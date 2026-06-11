c3ba09cf800fc5b606e6119ca057ff737565db92
Rishi Pomal <rishi417@gmail.com>
2026-06-11 11:16:06 +0530

    Added context path and CORS support

--- src/main/java/com/example/reactive/template/config/AppConfig.java
+++ src/main/java/com/example/reactive/template/config/AppConfig.java
@@ -11,6 +11,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.core.R2dbcEntityOperations;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableR2dbcRepositories(
@@ -47,5 +52,28 @@ public class AppConfig {
             ConnectionFactory connectionFactory) {
 
         return new R2dbcEntityTemplate(connectionFactory);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
